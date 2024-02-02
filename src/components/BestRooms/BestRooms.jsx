76a9d1be712db5fde2713a76397300494427257d
SARB <mrsinghbusiness05@gmail.com>
2024-02-02 15:19:47 +0530

    setting image -width/height

--- src/components/BestRooms/BestRooms.jsx
+++ src/components/BestRooms/BestRooms.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import rooms from '../../data/rooms.json';
 
 const BestRooms = () => {
   return (
-     <div className="bedroom-component">
-       <h2> BedRoom Component Cards   </h2>
+     <div className="flex flex-wrap">
+      {rooms.map((data,index) => {
+         return (
+          <>
+            <div className="w-[400px] h-[400px]  overflow-hidden bg-slate-500 m-4 p-4 " key = {index}>
+              <div className='w-[100%] h-[80%]  object-cover '> 
+                  <img className='w-full h-full'  src = {data.img}   alt = {data.name} />
+              </div>
+              <div> Name - {data.name} </div>
+              <div> Guests - {data.guests} </div>
+              <button> Book now ${data.price} </button>
+            </div>
+          </>
+         )
+      })}
      </div>
   )
 }
