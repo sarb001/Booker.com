b8560675576e3d1caccad5f93f1f6d67d389467b
SARB <mrsinghbusiness05@gmail.com>
2024-02-03 13:14:05 +0530

    adding selection grp

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -13,7 +13,7 @@ const Rooms = () => {
         {rooms.map((data) => {
          return (
           <>
-            <div className="w-[370px] h-[550px]  overflow-hidden bg-slate-500 m-4  "
+            <div className="w-[370px] h-[500px]  overflow-hidden bg-slate-500 m-4  "
             key = {data.id}>
 
               <div className='w-[100%] h-[60%]  object-cover '> 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,11 +4,75 @@ import { FaShoppingCart } from "react-icons/fa";
 import { MdCancel } from "react-icons/md";
 import { GiHamburgerMenu } from "react-icons/gi";
 
+import 'react-date-range/dist/styles.css';
+import 'react-date-range/dist/theme/default.css' ;
+import  { DateRangePicker } from  'react-date-range' ;
+import { addDays } from 'date-fns';
+import MOCKDATA from '../../data/MOCK_DATA (4).json';
+import { GrSubtractCircle } from "react-icons/gr";
+import { IoAddCircle } from "react-icons/io5";
 
 const Header = () => {
+
+   const [state,setState] = useState([
+     {
+       startDate : new Date(),
+       endDate   : new Date(),
+       key       : 'selection'
+     }
+   ]);
+
+    const [guests,setguests] = useState(
+      {
+        adult : 1,
+        children : 2,
+        room : 1
+      }
+    )
+
+
   return (
     <div className="header m-16">
-      efd
+      <div className='flex flex-row gap-20'>
+    
+        Using Date 
+        <div>
+           <DateRangePicker 
+            onChange={item => setState([item.selection])}
+            showSelectionPreview = {true}
+            moveRangeOnFirstSelection = {false}
+            months={1}
+            ranges={state}
+            direction='horizontal'
+           />
+        </div>
+
+        <div>
+          
+          <div> 
+            <span> Adult   </span> 
+            <button className='border-red-700 border-2 '>  <GrSubtractCircle /> </button> 
+              {guests.adult}
+            <button className='border-black   border-2 '> <IoAddCircle /> </button> 
+          </div>
+
+          <div> 
+          <span> Children   </span> 
+            <button  className='border-red-700 border-2 '>  <GrSubtractCircle /> </button> 
+                {guests.children} 
+            <button className='border-black border-2 '>  <IoAddCircle /> </button> 
+          </div>
+
+          <div>
+          <span> Room  </span> 
+           <button className='border-red-700 border-2 '>  <GrSubtractCircle /> </button> 
+            {guests.room}
+          <button className='border-black border-2 '>  <IoAddCircle /> </button> 
+          </div>
+
+        </div>
+
+    </div>
     </div>
   )
 }
