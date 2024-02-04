839e61a0a5b6123bf31b27c6d75a26f699f3b973
SARB <mrsinghbusiness05@gmail.com>
2024-02-05 04:43:12 +0530

    rooms component logic working

--- src/Pages/Rooms/Rooms.jsx
+++ src/Pages/Rooms/Rooms.jsx
@@ -11,40 +11,45 @@ const Rooms = () => {
 
   return (
     <div className="rooms-container m-16 ">
-
         <div className = 'flex flex-wrap'>
 
-          {rooms.map((data) => {
-          return (
-            <> 
-            
-            <div className='rooms-container' key = {data.id}>
-              <div className="w-[370px] h-[500px]  overflow-hidden bg-slate-500 m-4" >
+          {filterdataroom && filterdataroom.length > 0 ? 
+          (
+            filterdataroom.map((data) => {
+                return (
+                  <> 
+                    <div className='rooms-container' key = {data.id}>
+                      <div className="w-[370px] h-[500px]  overflow-hidden bg-slate-500 m-4" >
 
-                <div className='w-[100%] h-[60%]  object-cover '> 
-                    <img className='w-full h-full'  src = {data.img}   alt = {data.name} />
-                </div>
+                        <div className='w-[100%] h-[60%]  object-cover '> 
+                            <img className='w-full h-full'  src = {data.img}   alt = {data.name} />
+                        </div>
 
-                <div className='h-[40%] flex flex-col justify-center items-center'>
-                  <div className='text-2xl'> Name - {data.name} </div>
-                  <div className='text-2xl'> Adults - {data.adults} </div>
-                  <div className='text-2xl'> Children - {data.children} </div>
+                        <div className='h-[40%] flex flex-col justify-center items-center'>
+                          <div className='text-2xl'> Name - {data.name} </div>
+                          <div className='text-2xl'> Adults - {data.adults} </div>
+                          <div className='text-2xl'> Children - {data.children} </div>
 
-                  <div className='m-4'>
-                    <Link className = 'p-2  border-2 border-black rounded-xl'
-                    to = {`/room/${data.id}`}> 
-                    Book now ${data.price} 
-                    </Link>
-                  </div>
+                          <div className='m-4'>
+                            <Link className = 'p-2  border-2 border-black rounded-xl'
+                            to = {`/room/${data.id}`}> 
+                            Book now ${data.price} 
+                            </Link>
+                          </div>
 
-                </div>
-                
-              </div>
-            </div>
-
-            </>
-          )
-          })}
+                        </div>
+                        
+                      </div>
+                    </div>
+                  </>
+                )
+            }) 
+            ): (
+              <>
+                 <h2>  No Rooms Found </h2>  
+              </>
+            )
+          }
 
        </div>
     </div>
