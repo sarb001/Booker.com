2fa1eafb77ae0905d288875417845cd801a4d3a7
SARB <mrsinghbusiness05@gmail.com>
2024-02-04 15:24:42 +0530

    adults / children working

--- src/Reducers/FIlterSlice.jsx
+++ src/Reducers/FIlterSlice.jsx
@@ -14,12 +14,12 @@ export const FilterSlice = createSlice({
             console.log('state 1- ' , JSON.stringify(state.rooms));
             console.log('state 2- ',action.payload);
             state.rooms = state.rooms?.filter((room) => room.adults === action.payload)
-            console.log('state 3- ',JSON.stringify(state.rooms));
+            console.log(' adult state - ',JSON.stringify(state.rooms));
         },
-        // sortbychildrens : (state,action) => {
-        //     state.hotels = state.rooms?.filter((room) => room.children === action.payload)
-        //     console.log('state 3- ',JSON.stringify(state.rooms));
-        // }
+        sortbychildrens : (state,action) => {
+            state.rooms = state.rooms?.filter((room) => room.children === action.payload)
+            console.log(' children state-',JSON.stringify(state.rooms));
+        }
     }
 })
 
