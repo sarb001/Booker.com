746fde3f580d76878870f243cd9da79fd3905a9d
SARB <mrsinghbusiness05@gmail.com>
2024-02-12 07:58:24 +0530

    responsive gallery

--- src/Pages/Gallery/Gallery.jsx
+++ src/Pages/Gallery/Gallery.jsx
@@ -6,22 +6,22 @@ const Gallery = () => {
    const allimages = [
     {id : 1 , imgsrc : "/src/assests/Best-room-2.avif" , name : "first-image" },
     {id : 2 , imgsrc : "/src/assests/Best-room-3.avif" , name : "second-image" },
-    {id : 3 , imgsrc : "/src/assests/Best-Room.avif" , name : "third-image" },
-    {id : 4 , imgsrc : "/src/assests/contact-us.avif" , name : "fourth-image" },
-    {id : 5 , imgsrc : "/src/assests/penthouse.jpeg" , name : "fifth-image" },
-    {id : 6 , imgsrc : "/src/assests/spa.jpeg" , name : "sixth-image" },
+    {id : 3 , imgsrc : "/src/assests/contact-us.avif" , name : "fourth-image" },
+    {id : 4 , imgsrc : "/src/assests/penthouse.jpeg" , name : "fifth-image" },
+    {id : 5 , imgsrc : "/src/assests/spa.jpeg" , name : "sixth-image" },
    ]
 
 
   return (
     <div className="gallery">
-       <div className='w-[100%] h-[350px] lg:relative'>
-        <div className='text-4xl lg:absolute lg:top-36 lg:left-1/2 lg:text-white '> Gallery </div>
+       <div className='w-[100%] h-[300px] lg:relative'>
+         
+        <div className='text-4xl absolute top-48  left-[41%] text-white lg:top-28 lg:left-1/2 '> Gallery </div>
          <img className='w-full h-full object-cover' src = "/src/assests/gallery.avif" alt = "gallery" />
        </div>
 
        <div className="images-container m-16">
-         <div className='flex flex-wrap flex-row'>
+         <div className='flex flex-wrap flex-row md:gap-8 '>
             {allimages.map((data) => {
                return (
                 <>
@@ -33,9 +33,9 @@ const Gallery = () => {
                 </>
                )
             })}
-           <img src = ""  alt = ""/>
          </div>
        </div>
+
     </div>
   )
 }
